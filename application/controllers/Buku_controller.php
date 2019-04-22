519bce69b9b43d990d2a83daeac9cb0164d22634
denysugiarto <49837608+denysugiarto@users.noreply.github.com>
2019-04-22 23:31:47 +0700

    nambahin halaman utama

---
+++ application/controllers/Buku_controller.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Buku_controller extends CI_Controller
+{
+
+public function index(){
+$this->load->view("halaman_utama");
+
+}
+
+
+
+}
--- application/views/contoh/layout_header.php
+++ application/views/contoh/layout_header.php
@@ -1,8 +1,8 @@
 <header id="header" class="header">
     <div class="top-left">
         <div class="navbar-header">
-            <a class="navbar-brand" href="./"><img src="<?php echo base_url().'assets/images/logo.png'?>" alt="Logo"></a>
-            <a class="navbar-brand hidden" href="./"><img src="<?php echo base_url().'assets/images/logo2.png'?>" alt="Logo"></a>
+            <a class="navbar-brand" href=".."><img src="<?php echo base_url().'assets/images/logo.png'?>" alt="Logo"></a>
+            <a class="navbar-brand hidden" href=".."><img src="<?php echo base_url().'assets/images/logo2.png'?>" alt="Logo"></a>
             <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
         </div>
     </div>
